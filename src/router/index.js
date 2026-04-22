4dde5007ae20198b9fc4777da55f9ac9b838fddb
leiyu-5 <yu.lei5@transsion.com>
2026-04-22 18:04:34 +0800

    Fix router import to use createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
@@ -43,3 +43,4 @@ router.beforeEach((to, from, next) => {
 })
 
 export default router
+
